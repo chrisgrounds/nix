34e8c12e95db144fcfb2536d2393461d82ffdd8a
chrisgrounds <iamchrisgrounds@gmail.com>
2025-11-07 13:58:20 +0000

    extract zed

---
+++ zed.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }: {
+  programs.zed-editor = {
+    enable = true;
+    extraPackages = [ pkgs.nixd ];
+    extensions = [ "nix" "toml" "rust" "catppuccin" "catppuccin-icons" ];
+    userSettings = {
+      theme = "Catppuccin Mocha";
+      ui_font_size = 22;
+      buffer_font_size = 20;
+      hour_format = "hour24";
+      tab_bar = { show = true; };
+      tabs = { show_diagnostics = "errors"; };
+      indent_guides = {
+        enabled = true;
+        coloring = "indent_aware";
+      };
+      inlay_hints = { enabled = true; };
+      lsp = {
+        rust-analyzer = { binary = { path_lookup = true; }; };
+
+        nix = { binary = { path_lookup = true; }; };
+      };
+    };
+  };
+}
